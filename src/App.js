3a2571fc3a0ffde7e6702bf0849c4db92af4a43a
Taenggu99 <leetaeeun99@gmail.com>
2025-10-19 17:03:40 +0900

    feat(app): 빈 문자열 입력 시 0 출력

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import {Console} from "@woowacourse/mission-utils";
 class App {
   async run() {
 
+    // 사용자 입력 받기
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.') ;
-    Console.print(`결과 :   ${input} `);
     
-   }
+
+    // 빈 문자열 처리
+    if (input === ""){
+      Console.print("결과 : 0");
+      return;
+    }
+  }
 }
 
 export default App;
