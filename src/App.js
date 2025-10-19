70fb58c7dd8a691b418b8efe19bf1ce0d01e91ad
Taenggu99 <leetaeeun99@gmail.com>
2025-10-19 17:50:41 +0900

    feat(app): 기본 구분자(쉼표, 콜론) 처리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import {Console} from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-
     // 사용자 입력 받기
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.') ;
-    
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
 
     // 빈 문자열 처리
-    if (input === ""){
+    if (input === "") {
       Console.print("결과 : 0");
       return;
     }
 
     // 공백만 있는 입력 처리
-    if (!input.trim()){
-      throw new Error ("ERROR] 입력이 비어있거나 공백입니다.");
+    if (!input.trim()) {
+      throw new Error("[ERROR] 입력이 공백입니다.");
     }
-  }
-}
 
+    // 기본 구분자 설정
+    const delimiter = /,|;/;
+
+
+
+  }
+
+
+
+}
 export default App;
